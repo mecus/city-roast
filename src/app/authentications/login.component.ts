b900663684bf4fe2c0e7f6cbf3aed32633c3315c
MECUS <design@miscotech.co.uk>
2017-01-21 12:49:50 +0000

    adding pages photos

--- src/app/authentications/login.component.ts
+++ src/app/authentications/login.component.ts
@@ -11,12 +11,18 @@ import { AuthService } from './auth-service';
         
         <div class="lp-page-container">
             <div class="lp-page-head">
-                <img src="assets/about-us.jpg"  alt="">
+                <img src="assets/slider-access-control-jpg.jpeg"  alt="">
                 <div class="lp-page-title">
                 <h1>Login</h1>
                 </div>
             </div>
+    
             <div class="container form-content">
+            <div class="container login-head">
+                <h4>Login</h4>
+                <p>Please login with your email and password</p>
+            </div>
+            
                 <form novalidates [formGroup]="user" (ngSubmit)="loginUser(user.value)">
                     <div class="form-group">
                         <label for="email">Email</label>
@@ -26,7 +32,10 @@ import { AuthService } from './auth-service';
                         <label for="password">Password</label>
                         <input type="password" class="form-control" formControlName="password" placeholder="password">
                     </div>
-                    <button type="submit" class="btn btn-primary">Login</button>
+                    <div class="container but-ton">
+                        <button type="submit" class="btn btn-primary">Login</button>
+                        or <span routerLink="/signup">Sign up</span>
+                    </div>
                 </form>
             </div>
         </div>
@@ -42,6 +51,7 @@ import { AuthService } from './auth-service';
         img{
             width: 100%;
             opacity: 0.1;
+            margin-top: -200px;
         }
         .form-content{
             max-width: 600px;
@@ -56,15 +66,31 @@ import { AuthService } from './auth-service';
             color: #fff;
             font-size: 2rem;
         }
+        .lp-page-title h1{
+            color: lightslategray;
+            font-size: 1.5rem;
+            margin-right: 15px;
+        }
         .form-content{
             min-height: 500px;
+        }
+        .login-head{
+            text-align: center;
+            margin-bottom: 30px;
+        }
+        .login-head p{
+            color: lightslategray;
+        }
+        .but-ton{
+            margin: 50px 0px;
+            text-align: center;
         }
         `
     ]
 })
 export class LoginComponent implements OnInit {
     user:FormGroup;
-    constructor(private fb:FormBuilder, private authService:AuthService) {
+    constructor(private fb:FormBuilder, private authService:AuthService, private router:Router ) {
 
         this.user = fb.group({
             email: "",
--- src/app/pages/coffee/coffee.component.ts
+++ src/app/pages/coffee/coffee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-coffee',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./coffee.component.scss']
 })
 export class CoffeeComponent implements OnInit {
-
-  constructor() { }
+  products = [];
+  constructor(private producService:ProductService) { }
 
   ngOnInit() {
+    this.producService.getProduct()
+      .subscribe((products)=>{
+        this.products = products;
+      });
   }
 
 }
